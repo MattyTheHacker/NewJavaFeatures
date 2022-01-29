48e0174e18e0c6e71ea39122ffbdedc4c908bf99
Matty <matty258987@gmail.com>
2022-01-29 03:01:01 +0000

    instance example

--- src/instances/IncancesExample.java
+++ src/instances/IncancesExample.java
@@ -1,4 +1,18 @@
 package instances;
 
 public class IncancesExample {
+    public void instancesExample() {
+        AbstractFile abstractFile = new MusicFile();
+        useInstance(abstractFile);
+    }
+
+    private void useInstance(AbstractFile abstractFile) {
+        if (abstractFile instanceof MusicFile) {
+            ((MusicFile) abstractFile).playMusic();
+        }
+
+        if (abstractFile instanceof MusicFile musicFile) {
+            musicFile.playMusic();
+        }
+    }
 }
